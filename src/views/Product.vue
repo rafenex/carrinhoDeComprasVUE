475270b90a5e2640e7ca57e5b84250fde4e4e120
rafenex <dvr.rafa@gmail.com>
2022-10-04 15:30:35 -0300

    carrinho de compras

---
+++ src/views/Product.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="basket">
+    Product
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: 'Product',
+  
+    methods: {
+     
+    },
+   
+  }
+  </script>
+  
+  <style lang="scss">
+  
+
+  </style>
+  
